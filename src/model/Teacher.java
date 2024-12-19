a9957a533e3477b70b181d7a1b707486ce91dae8
abdel-kobbi <mr.abdelkhaliqkobbi@gmail.com>
2024-12-19 18:55:37 +0100

    Add teacher model

---
+++ src/model/Teacher.java
@@ -0,0 +1,37 @@
+package model;
+
+
+public class Teacher {
+    private static int idU = 0;
+    private String nom;
+    private int age, id;
+    private String module;
+
+    public Teacher(String nom, int age, String module){
+        this.id = ++idU;
+        this.nom = nom;
+        this.age = age;
+        this.module = module;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public String toString(){
+        return "ID: " + this.id + ", Nom: " + this.nom + ", Age: " + this.age + ", Module: " + this.module;
+    }
+
+}
