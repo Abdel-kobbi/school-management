e4bac2abb8fef08360d53d89b4b34f8846c519e1
abdel-kobbi <mr.abdelkhaliqkobbi@gmail.com>
2024-12-22 17:26:03 +0100

    add option information of school

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,39 +1,46 @@
 package controller;
 
 import java.util.Scanner;
+
+import model.School;
 import view.MainView;
 
 public class MainController {
     private static Scanner input;
+    private School school;
     private MainView mainView;
     private StudentController studentController;
     private TeacherController teacherController;
 
     public MainController(){
         input = new Scanner(System.in);
-        mainView = new MainView();
-        studentController = new StudentController();
-        teacherController = new TeacherController();
+        this.school = School.getSchool();
+        this.mainView = new MainView();
+        this.studentController = new StudentController();
+        this.teacherController = new TeacherController();
     }
 
     public void start() {
         int entry;
         do {
-            mainView.displayStartMenu();
+            this.mainView.displayStartMenu();
             entry = input.nextInt();
             switch (entry) {
                 case 1:
-                    teacherController.start();
+                    this.teacherController.start();
                     break;
                 case 2:
-                    studentController.start();
+                    this.studentController.start();
                     break;
                 case 3:
+                    this.mainView.displayInfoSchool(school.getTeachers().size(), school.getStudents().size());
+                    break;
+                case 4:
                     break;
                 default:
                     System.out.println("Choix invalide!");
             }
-        } while (entry != 3);
+        } while (entry != 4);
 
         System.out.println("Fin");
     }
